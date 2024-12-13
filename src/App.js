f3b179cba72b98912ad12d8c5b3055910cc71219
StarManJay <your-email@example.com>
2024-12-13 17:05:32 -0500

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import JobBoard from './components/JobBoard';
-import SignUp from './components/auth/SignUp';
-import PostJob from './components/jobs/PostJob';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<JobBoard />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/post-job" element={<PostJob />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<JobBoard />} />
+    </Routes>
   );
 }
 
